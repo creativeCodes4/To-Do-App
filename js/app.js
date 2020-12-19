c681f4d5d67ac49c4021a3b5e680c34902fa70be
creativeCodes4 <creative.codes@protonmail.com>
2020-12-19 07:13:33 -0800

    Vanilla JavaScript To-Do List App

--- js/app.js
+++ js/app.js
@@ -20,24 +20,42 @@ const addToDo = (item) =>{
     // Creating a new list item...
     const newLi = document.createElement('li')
 
+    // Creating icons div...
+    const iconDiv = document.createElement('div')
+
+    // Adding class to the icons div...
+    iconDiv.classList.add('icons')
+
     // Creating a Delete Button...
     const delBtn = document.createElement('i')
 
     // Adding Fontawesome to the element...
-    delBtn.className = 'fa fa-trash'
+    delBtn.classList.add('fa','fa-trash')
+
+    // Creating a Mark Button...
+    const markBtn = document.createElement('i')
+
+    // Adding Fontawesome to the element...
+    markBtn.classList.add('fa', 'fa-check-circle')
+    markBtn.id = 'mark'
+
+    // Appending buttons to the div...
+    iconDiv.appendChild(markBtn)
+    iconDiv.appendChild(delBtn)
+    
 
     // Creating a Text Node...
     const textContent = document.createTextNode(item)
 
     // Creating ClassNames...
-    newLi.className = 'item'
+    newLi.classList.add('item')
 
     // Adding the Delete btn attribute "role = delete "...
     delBtn.id = 'delete'
 
     // Appending to the LI...
     newLi.appendChild(textContent)
-    newLi.appendChild(delBtn)
+    newLi.appendChild(iconDiv)
 
     // Appending to the whole List...
     list.appendChild(newLi)
@@ -55,13 +73,24 @@ list.addEventListener('click', (e) => {
     }
 })
 
+list.addEventListener('click', (e) => {
+    const tElement = e.target
+    if (tElement.id == 'mark') {
+        const pElement = tElement.parentElement.parentElement
+        pElement.classList.toggle('marked')
+        tElement.classList.toggle('fa-circle-o')
+    }
+})
+
 
 // Removing ToDo...
 
 const deleteTodo = (item) => {
-    const listItem = item.parentElement
+    const listItem = item.parentElement.parentElement
     const pList = listItem.parentElement
-    item.style.marginLeft = '20px'
-    // console.log(pList);
-    pList.removeChild(listItem)
+    listItem.classList.add('trashed')
+    list.addEventListener('transitionend', (e) => {
+        e.preventDefault()
+        pList.removeChild(listItem)
+    })
 }
